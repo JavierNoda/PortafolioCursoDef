b696de033bb8f74a87ccc31f207c5511c627e391
JavierNoda <157540887+JavierNoda@users.noreply.github.com>
2024-04-18 20:47:21 -0400

    18/04/24

--- PortafolioCursoDef/Controllers/HomeController.cs
+++ PortafolioCursoDef/Controllers/HomeController.cs
@@ -1,66 +1,46 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PortafolioCursoDef.Models;
+using PortafolioCursoDef.Servicios;
 using System.Diagnostics;
+
 
 namespace PortafolioCursoDef.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IRepositorioProyectos repositorioProyectos;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,IRepositorioProyectos repositorioProyectos)
         {
             _logger = logger;
+            this.repositorioProyectos = repositorioProyectos;
         }
 
         public IActionResult Index()
         {
-            var proyectos = ObtenerProyectos().Take(3).ToList();
+            var proyectos = repositorioProyectos.ObtenerProyectos().Take(3).ToList();
             var modelo = new HomeIndexViewModel() { Proyectos = proyectos };
             return View(modelo);
         }
 
-        private new List<Proyecto> ObtenerProyectos()
+        public IActionResult Proyectos()
         {
-            return new List<Proyecto>() 
-            {  
-                new Proyecto()
-                {
-                    Titulo = "Bienes Raíces",
-                    Descripcion = "Página de bienes raices con HTML y css",
-                    Link = "https://bienes-raices-progcurso.netlify.app",
-                    ImagenURL = "/imagenes/proyectosBienesraicesPage.png"
-                },
-
-                new Proyecto()
-                {
-                    Titulo = "Freelancer",
-                    Descripcion = "Página de freenlancer con HTML y CSS",
-                    Link = "https://freelancer-progcurso.netlify.app",
-                    ImagenURL = "/imagenes/proyectosServicioFreelancerPage.png"
-                },
-
-                new Proyecto()
-                {
-                    Titulo = "Blog de Café",
-                    Descripcion = "Página Blog de café con HTML y CSS",
-                    Link = "https://blog-cafe-progcurso.netlify.app",
-                    ImagenURL = "/imagenes/proyectoVentaCafePage.png"
-                },
-
-                new Proyecto()
-                {
-                    Titulo = "GDLCAMP",
-                    Descripcion = "Web para evento de desarrollares con HTML y CSS",
-                    Link = "https://gdlwebcamp-progcurso.netlify.app",
-                    ImagenURL = "/imagenes/proyectoWebCampPage.png"
-                }
-
-            };
+            var proyectos = repositorioProyectos.ObtenerProyectos();
+            return View(proyectos);
         }
 
-        public IActionResult Privacy()
+        public IActionResult Contacto() {
+            return View();       
+        }
+
+        [HttpPost] //con esto se deja claro que solo funcionara cuando haya con una peticionde httpPost hacia homa/Contacto
+        public IActionResult Contacto(ContactoViewModel contactoViewModel)
         {
+            return RedirectToAction("Gracias");
+        }
+
+        public IActionResult Gracias () {
             return View();
         }
 
---
+++ PortafolioCursoDef/Servicios/RepositorioProyectos.cs
@@ -0,0 +1,51 @@
+﻿using PortafolioCursoDef.Models;
+
+namespace PortafolioCursoDef.Servicios
+{
+    public interface IRepositorioProyectos
+    {
+        List<Proyecto> ObtenerProyectos();
+    }
+
+    public class RepositorioProyectos : IRepositorioProyectos
+    {
+        public List<Proyecto> ObtenerProyectos()
+        {
+            return new List<Proyecto>()
+            {
+                new Proyecto()
+                {
+                    Titulo = "Bienes Raíces",
+                    Descripcion = "Página de bienes raices con HTML y css",
+                    Link = "https://bienes-raices-progcurso.netlify.app",
+                    ImagenURL = "/imagenes/proyectosBienesraicesPage.png"
+                },
+
+                new Proyecto()
+                {
+                    Titulo = "Freelancer",
+                    Descripcion = "Página de freenlancer con HTML y CSS",
+                    Link = "https://freelancer-progcurso.netlify.app",
+                    ImagenURL = "/imagenes/proyectosServicioFreelancerPage.png"
+                },
+
+                new Proyecto()
+                {
+                    Titulo = "Blog de Café",
+                    Descripcion = "Página Blog de café con HTML y CSS",
+                    Link = "https://blog-cafe-progcurso.netlify.app",
+                    ImagenURL = "/imagenes/proyectoVentaCafePage.png"
+                },
+
+                new Proyecto()
+                {
+                    Titulo = "GDLCAMP",
+                    Descripcion = "Web para evento de desarrollares con HTML y CSS",
+                    Link = "https://gdlwebcamp-progcurso.netlify.app",
+                    ImagenURL = "/imagenes/proyectoWebCampPage.png"
+                }
+
+            };
+        }
+    }
+}
